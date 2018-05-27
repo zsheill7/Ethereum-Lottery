39ce7c9e13a91b00279b7f601fce5bedd4bf261b
Zoe Sheill <zoe@Zoes-MacBook-Pro.local>
2018-05-26 20:28:05 -0700

    mocha tests

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -1,65 +1,71 @@
-const assert = require('assert');
-const ganache = require('ganache-cli');
-const Web3 = require('web3');
-
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require('../compile');
 
+const { interface, bytecode } = require("../compile");
+
+let lottery;
 let accounts;
-let inbox;
-
-let MAH_BOI = 'Yo mah boi';
 
 beforeEach(async () => {
-  // Get list of accounts
   accounts = await web3.eth.getAccounts();
 
-  // Use one account to deploy contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: [MAH_BOI]})
-    .send({ from: accounts[0], gas: '1000000'});
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
-describe('Inbox', () => {
-  it('deploys a contract', () => {
-    assert.ok(inbox.options.address);
+describe("Lottery Contract", () => {
+  it("deploys a contract", () => {
+    assert.ok(lottery.options.address);
   });
 
-  it('has a default message', async () => {
-    const message = await inbox.methods.message().call();
-    assert.equal(message, MAH_BOI);
+  it("allows one account to enter", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.02", "ether")
+    });
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(1, players.length);
   });
 
-  it('can change the message', async () => {
-    await inbox.methods.setMessage('bye')
-      .send({ from: accounts[0]});
-    const message = await inbox.methods.message().call();
-    assert.equal(message, 'bye');
+  it("allows multiple accounts to enter", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.02", "ether")
+    });
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei("0.02", "ether")
+    });
+    await lottery.methods.enter().send({
+      from: accounts[2],
+      value: web3.utils.toWei("0.02", "ether")
+    });
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+    assert.equal(3, players.length);
+  });
+
+  it("requires a minimum amount of ether to execute", async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 200
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
   });
 });
-
-// class Car {
-//   park() {
-//     return 'stopped';
-//   }
-//
-//   drive() {
-//     return 'vroom';
-//   }
-// }
-//
-// let car;
-//
-// beforeEach(() => {
-//   car = new Car();
-// });
-//
-// describe('Car', () => {
-//   it('it can park', () => {
-//     assert.equal(car.park(), 'stopped');
-//   });
-//
-//   it('it can drive', () => {
-//     assert.equal(car.drive(), 'vroom');
-//   });
-// })
